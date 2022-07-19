1571406dfbfd59b08e93e1bfe637c2f17f484859
Andrew Watson <creativedrewy@gmail.com>
2022-07-19 14:05:25 -0700

    And we'll all float on, okay

---
+++ app/src/main/java/com/solanamobile/buoysample/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.solanamobile.buoysample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.solanamobile.buoysample.ui.theme.BuoySampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BuoySampleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
