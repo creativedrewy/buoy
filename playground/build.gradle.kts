a1c2ed6de3562371668762bee2a24cfd87641c6c
Andrew Watson <creativedrewy@gmail.com>
2022-07-19 16:51:11 -0700

    Write initial v1 spec to attempt deserialization

--- playground/build.gradle.kts
+++ playground/build.gradle.kts
@@ -10,6 +10,7 @@ version = "1.0-SNAPSHOT"
 
 dependencies {
     implementation("com.squareup:kotlinpoet:1.12.0")
+    implementation("com.google.code.gson:gson:2.9.0")
 }
 
 tasks.withType<KotlinCompile>() {
--- playground/src/main/java/com/solanamobile/buoy/playground/Main.kt
+++ playground/src/main/java/com/solanamobile/buoy/playground/Main.kt
@@ -1,6 +1,6 @@
 package com.solanamobile.buoy.playground
 
-import com.squareup.kotlinpoet.*
+import java.io.File
 
 /**
  * ./gradlew :playground:run --args=<Path_to_json>
@@ -9,28 +9,32 @@ fun main(arguments: Array<String>) {
     println("----------------------------------------")
     println("")
 
-    val greeterClass = ClassName("", "Greeter")
-    val file = FileSpec.builder("com.solanamobile", "HelloWorld")
-        .addType(
-            TypeSpec.classBuilder("Greeter")
-                .primaryConstructor(
-                    FunSpec.constructorBuilder()
-                        .addParameter("name", String::class)
-                        .build()
-                )
-                .addProperty(
-                    PropertySpec.builder("name", String::class)
-                        .initializer("name")
-                        .build()
-                )
-                .addFunction(
-                    FunSpec.builder("greet")
-                        .addStatement("println(%P)", "Hello, \$name")
-                        .build()
-                )
-                .build()
-        )
-        .build()
-
-    file.writeTo(System.out)
+    File(arguments[0]).readLines().forEach {
+        println(it)
+    }
 }
+
+//    val greeterClass = ClassName("", "Greeter")
+//    val file = FileSpec.builder("com.solanamobile", "HelloWorld")
+//        .addType(
+//            TypeSpec.classBuilder("Greeter")
+//                .primaryConstructor(
+//                    FunSpec.constructorBuilder()
+//                        .addParameter("name", String::class)
+//                        .build()
+//                )
+//                .addProperty(
+//                    PropertySpec.builder("name", String::class)
+//                        .initializer("name")
+//                        .build()
+//                )
+//                .addFunction(
+//                    FunSpec.builder("greet")
+//                        .addStatement("println(%P)", "Hello, \$name")
+//                        .build()
+//                )
+//                .build()
+//        )
+//        .build()
+//
+//    file.writeTo(System.out)
