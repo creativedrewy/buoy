76b4e896967a7ce67e10e0631bfa20d95d930716
Andrew Watson <creativedrewy@gmail.com>
2022-07-19 19:42:54 -0700

    I don't know what I'm doing

--- AnchorGenerator/src/main/java/com/solanamobile/buoy/anchorgenerator/GeneratorPlugin.kt
+++ AnchorGenerator/src/main/java/com/solanamobile/buoy/anchorgenerator/GeneratorPlugin.kt
@@ -1,4 +1,21 @@
 package com.solanamobile.buoy.anchorgenerator
 
-class GeneratorPlugin {
+import org.gradle.api.DefaultTask
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.TaskAction
+
+class GeneratorPlugin: Plugin<Project> {
+    override fun apply(project: Project) {
+
+    }
+}
+
+class MyTask: DefaultTask() {
+
+    @TaskAction
+    fun doSomething() {
+        println(":: You have made a plugin!! ::")
+    }
+
 }
