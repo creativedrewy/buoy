0050671137f436cfeaf518f79ed2ab3726f09de8
Andrew Watson <creativedrewy@gmail.com>
2022-07-19 18:00:23 -0700

    A lil' more auto gen

--- playground/src/main/java/com/solanamobile/buoy/playground/Main.kt
+++ playground/src/main/java/com/solanamobile/buoy/playground/Main.kt
@@ -2,8 +2,10 @@ package com.solanamobile.buoy.playground
 
 import com.google.gson.Gson
 import com.solanamobile.buoy.playground.idlspec.IdlRootV1
+import com.solanamobile.web3.core.PublicKey
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
 import java.io.File
 
@@ -26,36 +28,42 @@ fun main(arguments: Array<String>) {
     )
 
     idlSource.instructions.forEach { instruction ->
-        mainClass.addFunction(
-            FunSpec.builder(instruction.name)
+        val functionBuilder = FunSpec.builder(instruction.name)
+
+        instruction.accounts.forEach { metaDescriptor ->
+            functionBuilder
+                .addParameter(metaDescriptor.name, PublicKey::class)
                 .build()
+        }
+
+        mainClass.addFunction(
+            functionBuilder.build()
         ).build()
     }
 
     contractFile.build().writeTo(System.out)
-}
 
-//    val greeterClass = ClassName("", "Greeter")
-//    val file = FileSpec.builder("com.solanamobile", "HelloWorld")
-//        .addType(
-//            TypeSpec.classBuilder("Greeter")
-//                .primaryConstructor(
-//                    FunSpec.constructorBuilder()
-//                        .addParameter("name", String::class)
-//                        .build()
-//                )
-//                .addProperty(
-//                    PropertySpec.builder("name", String::class)
-//                        .initializer("name")
-//                        .build()
-//                )
-//                .addFunction(
-//                    FunSpec.builder("greet")
-//                        .addStatement("println(%P)", "Hello, \$name")
-//                        .build()
-//                )
-//                .build()
-//        )
-//        .build()
-//
-//    file.writeTo(System.out)
+    val file = FileSpec.builder("com.solanamobile", "HelloWorld")
+        .addType(
+            TypeSpec.classBuilder("Greeter")
+                .primaryConstructor(
+                    FunSpec.constructorBuilder()
+                        .addParameter("name", String::class)
+                        .build()
+                )
+                .addProperty(
+                    PropertySpec.builder("name", String::class)
+                        .initializer("name")
+                        .build()
+                )
+                .addFunction(
+                    FunSpec.builder("greet")
+                        .addStatement("println(%P)", "Hello, \$name")
+                        .build()
+                )
+                .build()
+        )
+        .build()
+
+    file.writeTo(System.out)
+}
